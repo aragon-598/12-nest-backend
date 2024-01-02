6257faa96079fead0dbc1cbaad6a088de7326d98
Alejandro Aragon <aragondru155@gmail.com>
2024-01-01 23:01:33 -0600

    encriptar password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
+import * as bcryptjs from "bcryptjs";
 
 @Injectable()
 export class AuthService {
@@ -16,13 +17,22 @@ export class AuthService {
     
     try {
       //1 encriptar contraseñas
+      const {password, ...userData} = createUserDto;
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password,10),
+        ...userData
+      });
+      
+      
 
       //2 guardar usuario
 
       //3 generar jwt
-      const newUser = new this.userModel(createUserDto);
   
-      return await newUser.save();
+      await newUser.save();
+      const {password:_, ...user} = newUser.toJSON();
+
+      return user;
       
     } catch (error) {
       if(error.code === 11000){
